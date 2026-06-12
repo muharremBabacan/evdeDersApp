0763d092133b3cc7e675354562aa67f4751d4ad6
muharrembabacan <babacan.muharrem@gmail.com>
2026-06-12 17:38:25 +0300

    Fix request body reading in functions, update firebase project ID

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
+import io
 from firebase_functions import https_fn
 from app import app as flask_backend_app
 
 @https_fn.on_request()
 def flask_app(req: https_fn.Request) -> https_fn.Response:
-    with flask_backend_app.request_context(req.environ):
+    body_bytes = req.get_data()
+    environ = req.environ.copy()
+    environ['wsgi.input'] = io.BytesIO(body_bytes)
+    environ['CONTENT_LENGTH'] = str(len(body_bytes))
+    
+    with flask_backend_app.request_context(environ):
         return flask_backend_app.full_dispatch_request()
+
